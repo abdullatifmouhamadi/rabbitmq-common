d5c504ca6e1ca13e83ba34b7dc7920fa5741edd3
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2019-03-26 19:46:36 +0100

    Adding doc and src

---
+++ training/src/comsumer-ha.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import pika
+
+credentials = pika.PlainCredentials('guest', 'guest')
+argument_list = {'x-queue-master-locator':'random'}
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('34.217.176.249', 5672, '/', credentials, socket_timeout=300))
+channel = connection.channel()
+
+channel.queue_declare(queue='sample_test', durable=True)
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+channel.basic_consume(callback,
+                      queue='sample_test',
+                      no_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
